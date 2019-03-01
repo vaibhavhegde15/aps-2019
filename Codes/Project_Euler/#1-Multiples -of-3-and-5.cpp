bb04debc33ea0e9c31d5e7b356dfd2860edd0264
VAIBHAV S HEGDE <35395147+vaibhavhegde15@users.noreply.github.com>
2019-03-01 08:57:38 +0530

    Update #1-Multiples -of-3-and-5.cpp

--- Codes/Project_Euler/#1-Multiples -of-3-and-5.cpp
+++ Codes/Project_Euler/#1-Multiples -of-3-and-5.cpp
@@ -1,1 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
+long long sum(long long n,long long t){
+    long long p=(t-1)/n;
+    return (n*(p)*(p+1))/2;
+}
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        long long n;
+        cin >> n;
+        cout << sum(3,n)+sum(5,n)-sum(15,n) << endl;
+    }
+    return 0;
+}
