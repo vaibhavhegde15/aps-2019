f27060fb9fe1bd133ea4aa929cb95ead947057a8
VAIBHAV S HEGDE <35395147+vaibhavhegde15@users.noreply.github.com>
2019-03-01 08:59:42 +0530

    Update #3-Largest-prime-factor.cpp

--- Codes/Project_Euler/#3-Largest-prime-factor.cpp
+++ Codes/Project_Euler/#3-Largest-prime-factor.cpp
@@ -1,1 +1,27 @@
+#include <bits/stdc++.h>
+# include <math.h>
+using namespace std;
 
+int main() 
+{ 
+    long long int t,n,ans=0;
+    cin >> t;
+    while(t--){
+        cin >> n;
+        while (n%2 == 0){
+            ans = 2;
+            n = n/2; 
+        }
+        for (long long int i = 3; i <= sqrt(n); i = i+2){
+            while (n%i == 0){
+                ans = i;
+                n = n/i; 
+            } 
+        }
+        if (n > 2){
+            ans = n;
+        }
+        cout << ans << endl;
+    }
+    return 0; 
+} 
